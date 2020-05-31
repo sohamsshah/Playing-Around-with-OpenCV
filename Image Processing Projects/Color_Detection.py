67f5fdfd7679d28711b906f0702029bb75c661c6
sohamsshah <47717492+sohamsshah@users.noreply.github.com>
2020-05-31 20:06:29 +0530

    Update Color_Detection.py

--- Image Processing Projects/Color_Detection.py
+++ Image Processing Projects/Color_Detection.py
@@ -1,8 +1,7 @@
 # -*- coding: utf-8 -*-
 """
-Created on Sat Jun 29 23:24:24 2019
-
 @author: Soham Shah
+
 """
 import cv2
 import numpy as np
@@ -11,21 +10,19 @@ cap = cv2.VideoCapture(0)
 while(1):
     _,frame = cap.read()
     hsv = cv2.cvtColor(frame,cv2.COLOR_BGR2HSV)
+    
     lower_red = np.array([0,120,70])
     upper_red = np.array([10,255,255])
     mask1 = cv2.inRange(hsv, lower_red, upper_red)
- 
-
+    
     lower_red = np.array([170,120,70])
     upper_red = np.array([180,255,255])
+    
     mask2 = cv2.inRange(hsv,lower_red,upper_red)
- 
-
     mask1 = mask1+mask2
     
-    
-    
     res = cv2.bitwise_and(frame,frame,mask=mask1)
+    
     cv2.imshow('frame',frame)
     cv2.imshow('mask',mask1)
     cv2.imshow('res',res)
