f35d0a92e9ab0b576fbcf96b76940d2e42da82f6
sohamsshah <47717492+sohamsshah@users.noreply.github.com>
2020-05-31 20:15:36 +0530

    update commit

--- Object Detection Techniques/YOLO/yolo_object_detection.py
+++ Object Detection Techniques/YOLO/yolo_object_detection.py
@@ -1,7 +1,9 @@
 import cv2
 import numpy as np
 
+#download the 'yolov3.weights' and 'yolov3.cfg' files from the internet to load the model
 net = cv2.dnn.readNet("yolov3.weights", "yolov3.cfg")
+
 classes = []
 with open("coco.names", "r") as f:
     classes = [line.strip() for line in f.readlines()]
