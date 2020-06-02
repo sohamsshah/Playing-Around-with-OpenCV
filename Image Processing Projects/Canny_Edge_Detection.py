29fd25e9eda59d17f9218b099eb9e032c8b560ac
sohamsshah <sohamshah456@gmail.com>
2020-06-02 16:00:34 +0530

    add canny edge detection

---
+++ Image Processing Projects/Canny_Edge_Detection.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jun  2 15:57:03 2020
+
+@author: Soham Shah
+"""
+
+import cv2
+import numpy as np 
+  
+cap = cv2.imread('download (1).jpg')
+    
+hsv = cv2.cvtColor(cap, cv2.COLOR_BGR2HSV) 
+
+lower_red = np.array([30,150,50]) 
+
+upper_red = np.array([255,255,180]) 
+
+# create a red HSV colour boundary and
+# threshold HSV image 
+mask = cv2.inRange(hsv, lower_red, upper_red) 
+
+# Bitwise-AND mask and original image 
+
+res = cv2.bitwise_and(cap,cap, mask= mask) 
+edges = cv2.Canny(cap,100,200) 
+indices = np.where(edges != [0])
+coordinates = np.array([indices[0], indices[1]])
+print(coordinates)
+
+cv2.imshow('Original1.jpg',res)
+cv2.imshow("hey",edges)
+cv2.waitKey(2)
