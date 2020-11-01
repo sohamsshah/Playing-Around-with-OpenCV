62237618691b5f2e72dbb0dd33a38be4cd41231f
sohamsshah <sohamshah456@gmail.com>
2020-11-01 21:42:39 +0530

    reformating with black

--- Computer Vision Projects/Lane Detection/Lane_detection.py
+++ Computer Vision Projects/Lane Detection/Lane_detection.py
@@ -1,8 +1,8 @@
-
-#importings
+# importings
 import matplotlib.pylab as plt
 import cv2
 import numpy as np
+
 
 def region_of_interest(img, vertices):
     """
@@ -16,6 +16,7 @@ def region_of_interest(img, vertices):
     masked_image = cv2.bitwise_and(img, mask)
     return masked_image
 
+
 def draw_the_lines(img, lines):
     """
     Draws the lines on the image that are obtained by performing Hough Transform.
@@ -27,45 +28,58 @@ def draw_the_lines(img, lines):
 
     for line in lines:
         for x1, y1, x2, y2 in line:
-            cv2.line(blank_image, (x1,y1), (x2,y2), (0, 255, 0), thickness=10)
+            cv2.line(blank_image, (x1, y1), (x2, y2), (0, 255, 0), thickness=10)
 
     img = cv2.addWeighted(img, 0.8, blank_image, 1, 0.0)
     return img
 
-image = cv2.imread('road.png')
-image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-print(image.shape)
-height = image.shape[0]
-width = image.shape[1]
 
-#manual ROI
-region_of_interest_vertices = [
-    (0, height),
-    (width/2, height/2),
-    (width, height)
-]
+def image_processing_pipeline(image, region_of_interest_vertices):
+    # convert to gray_scale
+    gray_image = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
+    # canny edge detection
+    canny_image = cv2.Canny(gray_image, 100, 200)
+    # crop image
+    cropped_image = region_of_interest(
+        canny_image,
+        np.array([region_of_interest_vertices], np.int32),
+    )
 
-#convert to gray_scale
-gray_image = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
+    # draw lines
+    lines = cv2.HoughLinesP(
+        cropped_image,
+        rho=6,
+        theta=np.pi / 180,
+        threshold=160,
+        lines=np.array([]),
+        minLineLength=40,
+        maxLineGap=25,
+    )
 
-#canny edge detection
-canny_image = cv2.Canny(gray_image, 100, 200)
+    image_with_lines = draw_the_lines(image, lines)
+    return image_with_lines
 
-#crop image
-cropped_image = region_of_interest(canny_image,
-                np.array([region_of_interest_vertices], np.int32),)
 
-#draw lines
-lines = cv2.HoughLinesP(cropped_image,
-                        rho=6,
-                        theta=np.pi/180,
-                        threshold=160,
-                        lines=np.array([]),
-                        minLineLength=40,
-                        maxLineGap=25)
+def main():
+    image_path = "road.png"
+    image = cv2.imread(image_path)
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    height, width = image.shape[0], image.shape[1]
 
-image_with_lines = draw_the_lines(image, lines)
+    # manual ROI
+    region_of_interest_vertices = [
+        (0, height),
+        (width / 2, height / 2),
+        (width, height),
+    ]
 
-#display
-plt.imshow(image_with_lines)
-plt.show()
+    # image processing pipeline
+    result = image_processing_pipeline(image, region_of_interest_vertices)
+
+    # display
+    plt.imshow(result)
+    plt.show()
+
+
+if __name__ == "__main__":
+    main()
